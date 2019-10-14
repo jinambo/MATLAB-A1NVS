d08d04dbee7f8e6993c371e04b0ba06bcbe2f0c9
student FAI <student@C5-A218-13.ad.utb.cz>
2019-10-14 10:42:57 +0200

    fourth class

--- grafy/my_graf.m
+++ grafy/my_graf.m
@@ -1,5 +1,6 @@
-% fce y
 x = 0:pi/30:3*pi; 
+
+% fce y
 plot(x, 4*sin(x*2),'--b') ;
 
 hold on
@@ -10,6 +11,8 @@ plot(x, 5*cos(3*x), '-r');
 % popis os
 xlabel('èas [s]');
 ylabel('y1, y2');
+
+% nadpis grafu
 title('Prùbìh funkcí');
 
 
